6819b7bb3e4c38a91cef62c0d8cc1d3dbb71075d
juliansand <sandoval.julian10@gmail.com>
2017-03-23 17:52:18 -0600

    commit practica 2

---
+++ practica1/test.py
@@ -0,0 +1,26 @@
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from sistema.models import Credito, Debito, PagoServicio, Servicio, UserProfile, Transferencia
+
+
+class ServicioTest(TestCase):
+    def setUp(self):
+        User.objects.create(password="1234", username="prueba", email="prueba@gmail.com")
+        usuario = User.objects.get(username = "prueba")
+        Transferencia.objects.create(user_cuenta = "usuario1", user_cuenta2 = "usuario2", monto = '25.00')
+        UserProfile.objects.create(correo="hoa@gmail.com", nombre="hola", user = usuario)
+        Servicio.objects.create(servicio="luz")
+        Servicio.objects.create(servicio="cable")
+        cable = Servicio.objects.get(servicio = "cable")
+        PagoServicio.objects.create(cuenta_servicio="cuenta", tipo_servicio = cable, user_cuenta = "prueba", monto='10.00')        
+        user_cuenta = UserProfile.objects.get(correo = "hola@gmail.com")
+        Debito.objects.create(monto='10.00', descripcion="prueba", user_cuenta=user_cuenta)
+        Credito.objects.create(monto='10.00', descripcion="prueba", user_cuenta="user_cuenta")
+
+        
+    def test_credito(self):
+        credit = Credito.objects.get(descripcion="prueba")
+        self.assertNotEqual(credit.monto, '0.00')
+        self.assertIsNotNone(credit.user_cuenta)
