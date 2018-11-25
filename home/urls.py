172c2852ab03ff9dba06a397d9fd3ff14e98b833
Deymar Torrez <deytorrez7software@gmail.com>
2018-11-25 16:49:11 -0400

    eliminado

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(regex=r'^about/$', view=views.AboutView.as_view(), name='about'),
-    url(regex=r'^bienvenida/$', view=views.BienvenidaView.as_view(), name='bienvenida'),
-]
